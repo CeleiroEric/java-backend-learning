2a7853e194c7b38b9caa35a633f5db0bf0b7492e
CeleiroEric <celeiroericwork@gmail.com>
2026-01-21 03:15:01 +0100

    added email validation to InputUtil.java

--- InputUtil.java
+++ InputUtil.java
@@ -2,30 +2,52 @@ import java.util.Scanner;
 public class InputUtil {
     private static final Scanner sc = new Scanner(System.in);
 
-    public static String leerString(String mensaje){
+    public static String leerString(String mensaje) {
         System.out.println(mensaje);
-        return sc.nextLine();
+        return sc.nextLine().trim();
     }
-    public static int leerInt(String mensaje){
+
+    public static int leerInt(String mensaje) {
         int valor;
-        while(true){
+        while (true) {
             System.out.println(mensaje);
-            try{
-                valor = Integer.parseInt(sc.nextLine());
+            try {
+                valor = Integer.parseInt(sc.nextLine().trim());
                 break;
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Introduzca un numero correcto por favor:");
             }
         }
         return valor;
     }
-    public static int leerInt(String mensaje,int min,int max){
+
+    public static int leerInt(String mensaje, int min, int max) {
         int valor;
-        while(true){
+        while (true) {
             valor = leerInt(mensaje);
-            if(valor >= min && valor <= max) break;
+            if (valor >= min && valor <= max) break;
             System.out.println("Por favor, introduzca una opcion correcta.");
-            }
+        }
         return valor;
     }
+
+    public static boolean comprobarEmail(String email) {
+        return email.contains("@");
+    }
+
+    public static String leerEmail(String mensaje) {
+        String email;
+        while (true) {
+            System.out.println(mensaje);
+            email = sc.nextLine();
+            if (comprobarEmail(email)) {
+                break;
+            } else {
+                System.out.println("El email no es valido. ");
+            }
+        }
+        return email;
+    }
 }
+
+
