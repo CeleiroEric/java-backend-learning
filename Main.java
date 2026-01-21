1cda0a0b46ad5cefd8658ad21d2e2db841b74374
CeleiroEric <celeiroericwork@gmail.com>
2026-01-21 02:27:27 +0100

    Refactor Main.java to use InputUtil and clean menu logic

--- Main.java
+++ Main.java
@@ -2,25 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Sistema sistema = new Sistema();
 
         int opcion = 0;
         System.out.println("¡Bienvenido!");
         while(opcion!=3){
-            System.out.println("Elija una opcion:");
             System.out.println("1.Crear usuario.");
             System.out.println("2.Listar usuarios");
             System.out.println("3.Salir del programa");
-            opcion = sc.nextInt();
-            sc.nextLine();
+            opcion = InputUtil.leerInt("Elija una opcion: ",1,3);
+
 
             switch(opcion){
                 case 1:
-                    System.out.println("Introduzca el nombre del usuario:");
-                    String nombre = sc.nextLine();
-                    System.out.println("Introduzca el email del usuario:");
-                    String email = sc.nextLine();
+                    String nombre = InputUtil.leerString("Introduzca el nombre del usuario: ");
+                    String email = InputUtil.leerString("Introduzca el email del usuario: ");
                     Usuario usuario = new Usuario(nombre, email);
                     sistema.agregarUsuario(usuario);
                     System.out.println("¡El usuario ha sido creado correctamente!");
